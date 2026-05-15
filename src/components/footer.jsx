c924b52926b95b6eb994c72a0694722c2a8feea1
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-05-15 10:50:55 +0300

    feat: add footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="bg-[#042C53] py-8 px-12 mt-auto">
       <div className="flex flex-row items-center justify-between gap-2 text-center">
         <div className="font-[Cinzel_Decorative] text-2xl font-bold text-white/60 tracking-wide">
-          W<span className="text-green-light">L</span>
+          <span>WL</span>
         </div>
         <p className="flex text-[11px] gap-1 font-[Inter] tracking-widest uppercase text-white/60 mt-1">
           Images by
